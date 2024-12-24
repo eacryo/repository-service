4d9a7f0f7392ecb8d48972889b9fa2fc9b3faee3
EACRYO <eacryo@gmail.com>
2024-12-24 22:48:19 +0800

    init project

---
+++ src/main/java/com/eacryo/repositoryservice/controller/RepositoryReduceController.java
@@ -0,0 +1,19 @@
+package com.eacryo.repositoryservice.controller;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/repository")
+@EnableFeignClients
+public class RepositoryReduceController {
+
+    @PostMapping("/reduce")
+    public String reduce() {
+        return "reduce success";
+    }
+}
---
+++ src/test/java/com/eacryo/repositoryservice/RepositoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacryo.repositoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
